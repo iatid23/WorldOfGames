d6bb8fd382e167803c5303bfa844960c598e201b
iatid23@gmail.com <Ia1232310@!>
2022-12-21 15:15:49 +0200

    World Of games - second assignment - guesgame + start memory game

--- MainGame.py
+++ MainGame.py
@@ -1,6 +1,10 @@
 from Live import load_game, welcome, input_and_check_hello
-name = input_and_check_hello()
-print(welcome(name))
-load_game()
+from GuessGame import play
+from MemoryGame import generate_sequence
 
+#name = input_and_check_hello()
+#print(welcome(name))
+#load_game()
+#play(5)
 
+generate_sequence(9)
